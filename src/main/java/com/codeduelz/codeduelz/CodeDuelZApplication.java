2019e8eca72e8179469a3c0160bb206fd1580877
taksh-lade <takshlade982@gmail.com>
2025-12-18 18:48:35 +0530

    Initial commit

---
+++ src/main/java/com/codeduelz/codeduelz/CodeDuelZApplication.java
@@ -0,0 +1,13 @@
+package com.codeduelz.codeduelz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeDuelZApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeDuelZApplication.class, args);
+    }
+
+}
