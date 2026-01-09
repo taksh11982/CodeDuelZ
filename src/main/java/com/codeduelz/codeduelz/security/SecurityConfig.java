152b946c74d4b68bed9063ed8c9f5f17a028ac08
Madhurq <madhur.mu5@Gmail.com>
2026-01-09 21:57:40 +0530

    did a small change in leaderboard to match with ui

--- src/main/java/com/codeduelz/codeduelz/security/SecurityConfig.java
+++ src/main/java/com/codeduelz/codeduelz/security/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/leaderboard").permitAll()
                         .requestMatchers("/profile/*").permitAll()
                         .anyRequest().authenticated()
                 )
